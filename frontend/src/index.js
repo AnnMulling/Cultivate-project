504d78e0e4a8c888a317d88531e7365ae494203d
AnnPrim <Primpraowann@gmail.com>
2023-11-30 22:05:53 -0600

    session route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from "./store/session";
 
 import configureStore from './store';
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 function Root() {
   return (
