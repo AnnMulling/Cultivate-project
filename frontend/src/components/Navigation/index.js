645ab9d971bd54a17fe453c5ef8b718b636c7bfe
AnnPrim <Primpraowann@gmail.com>
2023-12-08 16:23:03 -0600

    added logo and favicon

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import logo from '../../assets/c-plain.png'
+
 import './Navigation.css';
+
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-  
+
   return (
     <ul className="nav-container">
         <li>
-          <NavLink exact to="/">Home</NavLink>
+          <NavLink exact to="/"><img src={logo} alt="logo" className="logo"/></NavLink>
         </li>
       {isLoaded && sessionUser && (
         <li>
