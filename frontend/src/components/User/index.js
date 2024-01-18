0a191a5b9420fff3a577858b14c55402862f04ea
AnnPrim <Primpraowann@gmail.com>
2024-01-18 11:14:36 -0600

    added date and time

--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, Link } from 'react-router-dom';
 import Sidebar from '../Navigation/Sidebar_';
+import DateTime from './DateTime';
 
 //img
 import kitty from '../../assets/splash/kitty.png'
@@ -19,13 +20,11 @@ export default function UserPage () {
         <div className="user-page">
             <Sidebar user={user} />
             <div className="user-inner">
-
                     <div className="user-hello">
                         <h1 style={{ fontSize: "55px", fontWeight: "lighter"}}>Hi, {user.firstName}!</h1>
-                        <p style={{ color: "grey", fontSize: "30px"}}>How are you doing today?</p>
-
+                        <p style={{ color: "#B4B4B4", fontSize: "30px"}}>How are you doing today?</p>
+                        <DateTime/>
                         <img style={{ marginLeft: "220px", position: "reletive"}} src={kitty} alt="kitty" />
-
 
                     </div>
              </div>
